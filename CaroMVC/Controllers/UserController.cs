5aa1c9e26d32f7c4d3cf7069ac97b82d7565de19
Slimaeus <nguyenhongthai28042002@gmail.com>
2022-09-20 22:25:27 +0700

    Update Login Action and View

--- CaroMVC/Controllers/UserController.cs
+++ CaroMVC/Controllers/UserController.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Model.ActionModels;
+using Model.RequestModels;
+using Service.APIClientServices;
 
 namespace CaroMVC.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserAPIClient _userAPIClient;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public UserController(IHttpContextAccessor httpContextAccessor)
+        public UserController(IUserAPIClient userAPIClient, IHttpContextAccessor httpContextAccessor)
         {
+            _userAPIClient = userAPIClient;
             _httpContextAccessor = httpContextAccessor;
         }
         public IActionResult Index()
@@ -16,7 +21,32 @@ namespace CaroMVC.Controllers
         }
         public IActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userAPIClient.Authenticate(loginModel.Input);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(returnUrl))
+                        return LocalRedirect(returnUrl);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid Credentials");
+            }
 
+            return View(loginModel);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterRequest loginModel, string returnUrl)
+        {
             return View();
         }
     }
---
+++ Model/ActionModels/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using Model.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ActionModels
+{
+    public class LoginModel
+    {
+        public string ReturnUrl { get; set; } = string.Empty;
+        public LoginRequest Input { get; set; } = new LoginRequest();
+    }
+}
