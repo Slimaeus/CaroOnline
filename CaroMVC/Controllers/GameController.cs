ff95bf697b568c0b1aac905dd9e608ddd1ac6230
Slimaeus <nguyenhongthai28042002@gmail.com>
2022-09-23 19:43:08 +0700

    Add GameController Add new Footer from Kha and Duc Add About from Duc Add new About nav item

---
+++ CaroMVC/Controllers/GameController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaroMVC.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: GameController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Play()
+        {
+            return View();
+        }
+        // GET: GameController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GameController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GameController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GameController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GameController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GameController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GameController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
