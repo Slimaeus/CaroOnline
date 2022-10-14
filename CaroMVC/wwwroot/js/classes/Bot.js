5e9a20f75493000503d84d0a30e377e3ea4bbceb
Slimaeus <nguyenhongthai28042002@gmail.com>
2022-10-14 10:30:05 +0700

    Update bot algorithm

--- CaroMVC/wwwroot/js/classes/Bot.js
+++ CaroMVC/wwwroot/js/classes/Bot.js
@@ -1,19 +1,28 @@
 ﻿class Bot {
     name;
+    moves;
+    userMoves;
     game;
     constructor(name, game) {
         this.name = name
+        this.moves = []
+        this.userMoves = [];
         this.game = game
     }
     createMove(userMove) {
-        let row = 0;
-        let col = 0;
+        this.userMoves.push(userMove);
+        let row = userMove.row;
+        let col = userMove.col;
+        const MIN = -1;
+        const MAX = 1;
         do {
-            row = Math.floor(Math.random() * (game.row - 1));
-            col = Math.floor(Math.random() * (game.col - 1));
+            row = row + Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+            col = col + Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
         }
         while (game.isMoveExist(row, col));
-        return new Move(row, col, bot.name);
+        const move = new Move(row, col, bot.name);
+        this.moves.push(move);
+        return move;
     }
 
 }
